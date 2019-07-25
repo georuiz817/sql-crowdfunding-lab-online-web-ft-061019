518667aded52338748efa7819824edea4d5a7ff1
George Ruiz <georuiz817@gmail.com>
2019-07-25 00:15:24 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE projects (ID INTENER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date  )
+CREATE TABLE projects (ID INTENER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT,)
