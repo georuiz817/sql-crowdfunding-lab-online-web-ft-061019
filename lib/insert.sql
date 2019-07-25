ed6eb40d6f263fb98fb11711333774e12440e796
George Ruiz <georuiz817@gmail.com>
2019-07-25 00:36:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,60 +1,60 @@
 INSERT INTO users (name, age)
-VALUES (brunhilda,24 );
+VALUES ('brunhilda',24 );
 
 INSERT INTO users (name, age)
-VALUES (basoon ,15 );
+VALUES ('basoon' ,15 );
 
 INSERT INTO users (name, age)
-VALUES (popeye ,50 );
+VALUES ('popeye' ,50 );
 
 INSERT INTO users (name, age)
-VALUES ( kyle,15 );
+VALUES ( 'kyle',15 );
 
 INSERT INTO users (name, age)
-VALUES (phteven , 30 );
+VALUES ('phteven' , 30 );
 
 INSERT INTO users (name, age)
-VALUES (mike ,1 );
+VALUES ('mike' ,1 );
 
 INSERT INTO users (name, age)
-VALUES (bubbles , 100 );
+VALUES ('bubbles' , 100 );
 
 INSERT INTO users (name, age)
-VALUES (cuddles , 21 );
+VALUES ('cuddles' , 21 );
 
 INSERT INTO users (name, age)
-VALUES (candle ,45 );
+VALUES ('candle' ,45 );
 
 INSERT INTO users (name, age)
-VALUES (martini ,28 );
+VALUES ('martini' ,28 );
 
 INSERT INTO users (name, age)
-VALUES (lemonpop , 28 );
+VALUES ('lemonpop' , 28 );
 
 INSERT INTO users (name, age)
-VALUES (clarinet , 100 );
+VALUES ('clarinet' , 100 );
 
 INSERT INTO users (name, age)
-VALUES (sara ,21 );
+VALUES ('sara' ,21 );
 
 INSERT INTO users (name, age)
-VALUES (orange ,4 );
+VALUES ('orange' ,4 );
 
 INSERT INTO users (name, age)
-VALUES (oatmeal , 73 );
+VALUES ('oatmeal' , 73 );
 
 INSERT INTO users (name, age)
-VALUES (alexa ,18 );
+VALUES ('alexa' ,18 );
 
 INSERT INTO users (name, age)
-VALUES (orlando , 42 );
+VALUES ('orlando' , 42 );
 
 INSERT INTO users (name, age)
-VALUES (mimi , 30 );
+VALUES ('mimi' , 30 );
 
 INSERT INTO users (name, age)
-VALUES (donner , 18 );
+VALUES ('donner' , 18 );
 
 INSERT INTO users (name, age)
-VALUES (prancer ,45 );
+VALUES ('prancer' ,45 );
 
