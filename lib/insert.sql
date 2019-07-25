0602a53b31c4c4a74b1bac17891378eec02feb63
George Ruiz <georuiz817@gmail.com>
2019-07-25 02:29:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
---users start
+-- users start 
 
 INSERT INTO users (name, age)
 VALUES ();
@@ -7,91 +7,57 @@ INSERT INTO users (name, age)
 VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('popeye' ,50 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ( 'kyle',15 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('phteven' , 30 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('mike' ,1 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('bubbles' , 100 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('cuddles' , 21 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('candle' ,45 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('martini' ,28 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('lemonpop' , 28 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('clarinet' , 100 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('sara' ,21 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('orange' ,4 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('oatmeal' , 73 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('alexa' ,18 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('orlando' , 42 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('mimi' , 30 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('donner' , 18 );
+VALUES ();
 
 INSERT INTO users (name, age)
-VALUES ('prancer' ,45 );
-
--- projects start 
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('squirtle_project','water', 5000, '3/15/19', '3/15/20');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('bulbasaur_project', 'grass', 3000, '2/20/20', '2/20/21');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('greninja_project', 'water', 6000, '6/16/26', '6/16/27');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('gastly_project', 'ghost', 22000, '10/10/31', '10/10/32');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('haunter_project', 'ghost', 20, '11/11/22', '11/11/23');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('metapod_project', 'grass', 2000, '12/12/31', '12/12/32');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('venasaur_project', 'grass', 1000, '12/12/20', '12/12/21');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('magikarp_project', 'water', 50000, '9/9/20', 
-'9/9/21');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('boo_project', 'ghost', 10000, '8/1/20', '8/1/21');
-
-INSERT INTO projects (title,category,funding_goal,start_date,end_date)
-VALUES ('goldeen_project', 'water', 40000, '1/1/50', '1/1/51');
-
+VALUES ();
 
 
