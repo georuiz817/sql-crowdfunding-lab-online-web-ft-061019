8d7553c220426011c473242001256d4d0cfff927
George Ruiz <georuiz817@gmail.com>
2019-07-25 02:50:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,8 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT title, pledges FROM projects ORDER BY title;"
+"SELECT title, pledge_amount FROM table_one INNER JOIN table_two ON table_one.column_name = table_two.column_name WHERE table_two.column_name = condition;
+;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
