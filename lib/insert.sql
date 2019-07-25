98992f081f556ab603b76e2cb92e867abf25fc57
George Ruiz <georuiz817@gmail.com>
2019-07-25 00:34:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,60 @@
 INSERT INTO users (name, age)
-VALUES ( , );
+VALUES (brunhilda,24 );
+
+INSERT INTO users (name, age)
+VALUES (basoon ,15 );
+
+INSERT INTO users (name, age)
+VALUES (popeye ,50 );
+
+INSERT INTO users (name, age)
+VALUES ( kyle,15 );
+
+INSERT INTO users (name, age)
+VALUES (phteven , 30 );
+
+INSERT INTO users (name, age)
+VALUES (mike ,1 );
+
+INSERT INTO users (name, age)
+VALUES (bubbles , 100 );
+
+INSERT INTO users (name, age)
+VALUES (cuddles , 21 );
+
+INSERT INTO users (name, age)
+VALUES (candle ,45 );
+
+INSERT INTO users (name, age)
+VALUES (martini ,28 );
+
+INSERT INTO users (name, age)
+VALUES (lemonpop , 28 );
+
+INSERT INTO users (name, age)
+VALUES (clarinet , 100 );
+
+INSERT INTO users (name, age)
+VALUES (sara ,21 );
+
+INSERT INTO users (name, age)
+VALUES (orange ,4 );
+
+INSERT INTO users (name, age)
+VALUES (oatmeal , 73 );
+
+INSERT INTO users (name, age)
+VALUES (alexa ,18 );
+
+INSERT INTO users (name, age)
+VALUES (orlando , 42 );
+
+INSERT INTO users (name, age)
+VALUES (mimi , 30 );
+
+INSERT INTO users (name, age)
+VALUES (donner , 18 );
+
+INSERT INTO users (name, age)
+VALUES (prancer ,45 );
 
