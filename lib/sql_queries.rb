2bfc6043dd6be187c0f8d3980f147e37d3801c88
George Ruiz <georuiz817@gmail.com>
2019-07-25 02:53:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,8 +6,12 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT title, pledge_amount FROM  INNER JOIN table_two ON table_one.column_name = table_two.column_name WHERE table_two.column_name = condition;
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name "SELECT projects.title, SUM(pledges.amount)
+  FROM projects
+  INNER JOIN pledges
+  ON projects.id = pledges.project_id
+  GROUP BY projects.id
+  ORDER BY projects.title;"
 ;"
 end
 
